7d63982e6b1b2a4c8ea70d8f5f97e5b7d1b675cb
pawansahani <sahanipawan84@gmail.com>
2021-03-16 23:47:23 +0530

    Add files via upload

---
+++ Q2/RecentAccounts/RecentAccountsHelper.js
@@ -0,0 +1,12 @@
+({
+	loadAccounts : function(component,event,helper) {
+		var action = component.get('c.getAccounts');
+        action.setCallback(this,function(response){
+            var state = response.getState();
+            if(state == 'SUCCESS'){
+                component.set('v.data',response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+	}
+})
